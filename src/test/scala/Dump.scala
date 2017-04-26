23193a603f483e320d4477f73d7ff742edea7c00
Hannes Karppila <hannes.karppila@gmail.com>
2017-04-26 13:28:55 +0300

    Add a few test cases for dumping only

--- src/test/scala/Dump.scala
+++ src/test/scala/Dump.scala
@@ -1,7 +1,25 @@
-import EasyJson.{EasyJson, ParseError}
+import EasyJson.{EasyJson, JsonData, ParseError}
 import org.scalatest._
 
 class DumpTest extends FlatSpec {
+    "EasyJson" should "be able to dump an empty list" in {
+        assert(EasyJson.dumps(JsonData(Vector[Any]())) == "[]")
+    }
+    it should "be able to dump an empty object" in {
+        assert(EasyJson.dumps(JsonData(Map[String,Any]())) == "{}")
+    }
+    it should "be able to dump a list of integers" in {
+        assert(EasyJson.dumps(JsonData(Vector(0,1,1,2,3,5))) == "[0,1,1,2,3,5]")
+    }
+    it should "be able to dump a list of floats" in {
+        assert(EasyJson.dumps(JsonData(Vector(1.02, 3.14))) == "[1.02,3.14]")
+    }
+    it should "be able to dump a mixed list" in {
+        assert(EasyJson.dumps(JsonData(Vector("a", 42, 3.14))) == "[\"a\",42,3.14]")
+    }
+}
+
+class LoadAndDumpTest extends FlatSpec {
     "EasyJson" should "be able to load and dump an empty list" in {
         assert(EasyJson.dumps(EasyJson.loads("[]")) == "[]")
     }
